00a29153915b650aaee11f822a15d0becf8e4cff
Himanshu Jain <jainmanshu@gmail.com>
2024-01-21 01:16:40 -0600

    minor style fixes

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -3,29 +3,45 @@ import resume from "../resume/Resume.pdf";
 function Navbar() {
   return (
     <header className="sticky top-0 z-50 p-2">
-      <ul className="flex flex-wrap justify-end items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+      <ul className="flex flex-wrap gap-2 justify-end items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="#about" className="hover:underline me-4 md:me-6">
+          <a
+            href="#about"
+            className="inline-flex items-center text-slate-700 px-5 py-2.5 text-xs font-medium text-center rounded-lg hover:bg-indigo-400 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          >
             About
           </a>
         </li>
         <li>
-          <a href="#education" className="hover:underline me-4 md:me-6">
+          <a
+            href="#education"
+            className="inline-flex items-center text-slate-700 px-5 py-2.5 text-xs font-medium text-center rounded-lg hover:bg-indigo-400 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          >
             Education
           </a>
         </li>
         <li>
-          <a href="#work" className="hover:underline me-4 md:me-6">
+          <a
+            href="#work"
+            className="inline-flex items-center text-slate-700 px-5 py-2.5 text-xs font-medium text-center rounded-lg hover:bg-indigo-400 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          >
             Experience
           </a>
         </li>
         <li>
-          <a href="#projects" className="hover:underline me-4 md:me-6">
+          <a
+            href="#projects"
+            className="inline-flex items-center text-slate-700 px-5 py-2.5 text-xs font-medium text-center rounded-lg hover:bg-indigo-400 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          >
             Projects
           </a>
         </li>
         <li>
-          <a target="_blank" href={resume} className="hover:underline">
+          <a
+            target="_blank"
+            href={resume}
+            className="inline-flex items-center text-slate-700 px-5 py-2.5 text-xs font-medium text-center rounded-lg hover:bg-indigo-400 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          >
             Resume
           </a>
         </li>
--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -36,7 +36,7 @@ function Work() {
               </div>
             </div>
             <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-              <div className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-30 h-6 mb-3 lg: -ml-8 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">
+              <div className="sm:absolute lg:-ml-8 left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-25 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">
                 Jun 2018 - Present
               </div>
               <div className="text-xl font-bold text-slate-900"></div>
@@ -87,7 +87,7 @@ function Work() {
               </div>
             </div>
             <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-              <div className="sm:absolute ld: -ml-8 left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-34 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">
+              <div className="sm:absolute lg:-ml-8 left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-25 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">
                 Jan 2018 - May 2018
               </div>
               <div className="text-xl font-bold text-slate-900"></div>
